5fd6f215c86499a4890ef414b5b30d0c06cc8a78
AndriiIshchenko <iscenkoa@gmail.com>
2024-12-16 15:54:15 +0200

    add say_hellow

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class UserProfile(models.Model):
     username = models.CharField()
-    password = models.CharField(max_length=)
+    password = models.CharField(max_length=50)
     email = models.EmailField()
     last_name = models.CharField()
     first_name = models.CharField()
@@ -10,3 +10,6 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.username
+    
+    def say_hello(self):
+        return f"Hello {self.first_name} {self.last_name}"
